56eba4d2f19e039069e58234908a42a35e835790
Vicki Foss <vfoss@users.noreply.github.com>
2023-04-23 19:41:02 -0600

    Allow custom model to accept base64-encoded image as input

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Tflite {
   detectObjectOnImage(args, callback) {
     TfliteReactNative.detectObjectOnImage(
       args['path'],
+      args['imageBinary'],  // base 64 encoded image
       args['model'] || "SSDMobileNet",
       args['imageMean'] != null ? args['imageMean'] : 127.5,
       args['imageStd'] != null ? args['imageStd'] : 127.5,
